c7499ae303fcaf4d172c7aa92ffdf605e7ff6821
AlexLig <ligkalex@gmail.com>
2019-06-06 01:53:20 +0300

    extract studentform into seperate component

---
+++ src/components/StudentForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <form id="StudentForm" @submit.prevent="onSubmit">
+    <label for="name">Όνομα:</label>
+    <input
+      v-model="name"
+      type="text"
+      name="name"
+      required
+      placeholder="π.χ. Γιάννης Χιοννίδης"
+    />
+
+    <label for="am">Αριθμός Μητρώου:</label>
+    <input
+      v-model="am"
+      type="text"
+      name="am"
+      required
+      placeholder="π.χ 31337"
+    />
+
+    <label for="email">Email:</label>
+    <input
+      v-model="email"
+      type="email"
+      name="email"
+      required
+      placeholder="π.χ ibendtheknee@danny.com"
+    />
+
+    <label for="password">Κωδικός:</label>
+    <input
+      v-model="password"
+      type="password"
+      name="password"
+      required
+      placeholder="Είσαγετε τον κωδικό που επιθυμείτε"
+    />
+
+    <input class="form-button" type="submit" value="Αποθήκευση" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "StudentForm",
+  data() {
+    return {
+      name: null,
+      am: null,
+      email: null,
+      password: null
+    };
+  },
+  methods: {
+    onSubmit() {
+      const student = {
+        name: this.name,
+        am: this.am,
+        email: this.email,
+        password: this.password
+      };
+      this.$emit("student-submitted", student);
+      this.name = null;
+      this.am = null;
+      this.email = null;
+      this.password = null;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped src="@/assets/styles/_form.scss"></style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,39 +1,19 @@
 <template>
-  <form id="StudentForm">
-    <label for="name">Όνομα:</label>
-    <input
-      type="text"
-      name="name"
-      required
-      placeholder="π.χ. Γιάννης Χιοννίδης"
-    />
-
-    <label for="am">Αριθμός Μητρώου:</label>
-    <input type="text" name="am" required placeholder="π.χ 31337" />
-
-    <label for="email">Email:</label>
-    <input
-      type="email"
-      name="email"
-      required
-      placeholder="π.χ ibendtheknee@danny.com"
-    />
-
-    <label for="password">Κωδικός:</label>
-    <input
-      type="password"
-      name="password"
-      required
-      placeholder="Είσαγετε τον κωδικό που επιθυμείτε"
-    />
-
-    <input class="form-button" type="submit" value="Αποθήκευση" />
-  </form>
+  <StudentForm @student-submitted="handleSubmit"></StudentForm>
 </template>
 
 <script>
+import StudentForm from "@/components/StudentForm.vue";
 export default {
-  name: "signup"
+  name: "signup",
+  components: {
+    StudentForm
+  },
+  methods: {
+    handleSubmit(student) {
+      console.log(student);
+    }
+  }
 };
 </script>
 
