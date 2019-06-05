ee000261a20d9a6d95b9ef4181684f37c337bdd8
AlexLig <ligkalex@gmail.com>
2019-06-05 14:11:34 +0300

    normalize css and basic grid view on #app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <header class="appbar">Hello there</header>
+    <main class="content">
+      <router-view/>
+    </main>
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" >
+@import "@/assets/styles/normalize.scss";
+html,
+body,
+#app {
+  width: 100vw;
+  height: 100vh;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  display: grid;
+  grid-template-rows: 3rem auto;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.appbar {
+  background-color: blue;
+}
+
+.content {
+  overflow-y: auto;
 }
 </style>
