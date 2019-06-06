12db458acfa5c6de1017dca8cf0c7a39e1ce15a6
AlexLig <ligkalex@gmail.com>
2019-06-07 00:52:13 +0300

    Update LabRegistration.vue

--- src/views/LabRegistration.vue
+++ src/views/LabRegistration.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>labReg</div>
+  <form action>
+    <label for="select-course">Βήμα 1:</label>
+    <select id="select-course">
+      <option value>Επιλέξτε Μάθημα</option>
+      <option value="dog">Dog</option>
+      <option value="cat">Cat</option>
+      <option value="hamster">Hamster</option>
+      <option value="parrot">Parrot</option>
+      <option value="spider">Spider</option>
+      <option value="goldfish">Goldfish</option>
+    </select>
+    <label for="select-course">Βήμα 2:</label>
+    <select id="select-course">
+      <option value>Επιλέξτε Εργαστήριο</option>
+      <option value="dog">
+        <b>a</b>
+      </option>
+      <option value="cat">Cat</option>
+      <option value="hamster">Hamster</option>
+      <option value="parrot">Parrot</option>
+      <option value="spider">Spider</option>
+      <option value="goldfish">Goldfish</option>
+    </select>
+    <input class="form-button" type="submit" value="Αποθήκευση">
+    <input class="form-button" type="reset" value="Ακύρωση">
+  </form>
 </template>
 
 <script>
@@ -7,3 +32,7 @@ export default {
   name: "labRegistration"
 };
 </script>
+
+<style lang="scss" scoped src="@/assets/styles/_form.scss">
+</style>
+
