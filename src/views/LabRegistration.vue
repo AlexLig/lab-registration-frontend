2c272e294d388730410dab60008bb618b67ae3be
AlexLig <ligkalex@gmail.com>
2019-06-09 19:20:57 +0300

    update styles. goback on form reset.

--- src/views/LabRegistration.vue
+++ src/views/LabRegistration.vue
@@ -1,24 +1,28 @@
 <template>
-  <form action>
-    <section>
-      <label for="select-course">Βήμα 1: Επιλέξτε μάθημα</label>
-      <select id="select-course" v-model="selectedCourse">
-        <option v-for="course in courses" v-bind:key="course.id" :value="course">{{ course.name }}</option>
-      </select>
-    </section>
-    <section class="lab-selection" v-if="!!selectedCourse">
-      <label for="select-course">Βήμα 2: Επιλέξτε εργαστήριο</label>
-      <select id="select-course" v-model="selectedLab" v-if="labs && labs.length">
-        <option
-          v-for="lab in labs"
-          v-bind:key="lab.id"
-          :value="lab.id"
-        >{{ lab.dayIso | isoDayToGreek }} {{ lab.startTime | fourDigitsHour }}-{{ lab.finishTime | fourDigitsHour }}</option>
-      </select>
-      <p v-else>Δέν βρέθηκαν εργαστήρια για το μάθημα {{selectedCourse.name}}</p>
-    </section>
-    <input class="form-button" type="submit" value="Αποθήκευση">
-    <input class="form-button" type="reset" value="Ακύρωση">
+  <form action @reset.prevent="goBack">
+    <div class="selections">
+      <section>
+        <label for="select-course">Βήμα 1: Επιλέξτε μάθημα</label>
+        <select id="select-course" v-model="selectedCourse">
+          <option v-for="course in courses" v-bind:key="course.id" :value="course">{{ course.name }}</option>
+        </select>
+      </section>
+      <section class="lab-selection" v-if="!!selectedCourse">
+        <label for="select-course">Βήμα 2: Επιλέξτε εργαστήριο</label>
+        <select id="select-course" v-model="selectedLab" v-if="labs && labs.length">
+          <option
+            v-for="lab in labs"
+            v-bind:key="lab.id"
+            :value="lab.id"
+          >{{ lab.dayIso | isoDayToGreek }} {{ lab.startTime | fourDigitsHour }}-{{ lab.finishTime | fourDigitsHour }}</option>
+        </select>
+        <p v-else>Δέν βρέθηκαν εργαστήρια για το μάθημα {{selectedCourse.name}}</p>
+      </section>
+    </div>
+    <div class="form-buttons">
+      <input class="form-button" type="submit" value="Αποθήκευση">
+      <input class="form-button" type="reset" value="Ακύρωση">
+    </div>
   </form>
 </template>
 
@@ -61,6 +65,9 @@ export default {
       });
       const labs = await res.json();
       this.labs = labs;
+    },
+    goBack() {
+      this.$router.go(-1);
     }
   },
   filters: {
@@ -85,14 +92,21 @@ export default {
 
 <style lang="scss" scoped >
 @import "@/assets/styles/_form.scss";
-option {
-  width: 20rem;
-}
 label {
   margin: 1rem 0;
-  font-size: 1.2rem
+  font-size: 1.2rem;
 }
-.lab-selection{
+.lab-selection {
   margin-bottom: 1rem;
+}
+.selections {
+  min-height: 12rem;
+}
+.form-buttons {
+  display: flex;
+  justify-content: space-evenly;
+}
+input[type="reset"] {
+  background-color: lightgray;
 }
 </style>
