7cba03fab3feaee40297b4a5e1b0dc6633fec377
AlexLig <ligkalex@gmail.com>
2019-06-12 11:28:47 +0300

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,13 @@
 <template>
-  <form id="Login">
+  <form id="Login" @submit.prevent="login">
     <label for="email">Email:</label>
     <input
       type="email"
       name="email"
       required
       placeholder="π.χ ikilledmyaunt@ironthrone.com"
-    />
+      v-model="email"
+    >
 
     <label for="password">Κωδικός:</label>
     <input
@@ -14,15 +15,30 @@
       name="password"
       required
       placeholder="Είσαγετε τον κωδικό που επιθυμείτε"
-    />
+      v-model="password"
+    >
 
-    <input class="form-button" type="submit" value="Είσοδος" />
+    <input class="form-button" type="submit" value="Είσοδος">
   </form>
 </template>
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      this.$store.dispatch("login", {
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
 };
 </script>
 <style lang="scss" scoped src="@/assets/styles/_form.scss"></style>
