6455a4a8ea61cf2aa7e01aa43fcfd314f49db1ef
AlexLig <ligkalex@gmail.com>
2019-06-13 19:37:50 +0300

    link to signup.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,28 @@
 <template>
-  <form id="Login" @submit.prevent="login">
-    <label for="email">Email:</label>
-    <input
-      type="email"
-      name="email"
-      required
-      placeholder="π.χ ikilledmyaunt@ironthrone.com"
-      v-model="email"
-    />
+  <div>
+    <form id="Login" @submit.prevent="login">
+      <label for="email">Email:</label>
+      <input
+        type="email"
+        name="email"
+        required
+        placeholder="π.χ ikilledmyaunt@ironthrone.com"
+        v-model="email"
+      />
 
-    <label for="password">Κωδικός:</label>
-    <input
-      type="password"
-      name="password"
-      required
-      placeholder="Είσαγετε τον κωδικό που επιθυμείτε"
-      v-model="password"
-    />
+      <label for="password">Κωδικός:</label>
+      <input
+        type="password"
+        name="password"
+        required
+        placeholder="Είσαγετε τον κωδικό που επιθυμείτε"
+        v-model="password"
+      />
 
-    <input class="form-button" type="submit" value="Είσοδος" />
-  </form>
+      <input class="form-button" type="submit" value="Είσοδος" />
+    </form>
+    <router-link to="signup">Κάντε εγγραφή εδώ</router-link>
+  </div>
 </template>
 
 <script>
